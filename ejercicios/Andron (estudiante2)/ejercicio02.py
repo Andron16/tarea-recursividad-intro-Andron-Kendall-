7baaab3535e93b7ec26f736cfd6bbeb33a6ba945
Andron <andronumana050@gmail.com>
2026-06-08 21:34:15 -0600

    se resuelve el ejercicio02 mediante recursividad de pila

--- ejercicios/Andron (estudiante2)/ejercicio02.py
+++ ejercicios/Andron (estudiante2)/ejercicio02.py
@@ -13,3 +13,16 @@ Casos de prueba:
 • contar_pares(12040) debe retornar 4 
 • contar_pares(7) debe retornar 0
 '''
+
+def contar_pares(numero):
+    numero = abs(numero)
+    if numero == 0:
+        return 0
+    if ((numero % 10) % 2) == 0:
+        return 1 + contar_pares(numero // 10)
+    return contar_pares(numero // 10)
+
+print(contar_pares(2486))
+print(contar_pares(13579))
+print(contar_pares(12040))
+print(contar_pares(7))
