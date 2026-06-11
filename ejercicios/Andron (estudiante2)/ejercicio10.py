515fa83f804e530b4034436a05afe4d0d51176db
Andron <andronumana050@gmail.com>
2026-06-10 21:31:58 -0600

    se resuelve el ejercicio10 mediante recursividad de pila, ejercicios de estudiante 2 terminados.

--- ejercicios/Andron (estudiante2)/ejercicio10.py
+++ ejercicios/Andron (estudiante2)/ejercicio10.py
@@ -12,3 +12,16 @@ Casos de prueba:
 • comprimir_repetidos([1, 2, 3, 4]) debe retornar [[1, 1], [2, 1], [3, 1], [4, 1]] 
 • comprimir_repetidos([]) debe retornar []
 '''
+
+def comprimir_repetidos(lista):
+    if lista == []:
+        return []
+    resto = comprimir_repetidos(lista[1:])
+    if resto != [] and resto[0][0] == lista[0]:
+        return [[lista[0], resto[0][1] + 1]] + resto[1:]
+    return [[lista[0], 1]] + resto
+
+print(comprimir_repetidos([1, 1, 1, 2, 2, 3, 1, 1]))
+print(comprimir_repetidos([5, 5, 5, 5]))
+print(comprimir_repetidos([1, 2, 3, 4]))
+print(comprimir_repetidos([]))
