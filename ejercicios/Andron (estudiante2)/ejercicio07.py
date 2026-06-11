662ee8653490c7e165a4a96d45109199d39b22b0
Andron <andronumana050@gmail.com>
2026-06-10 20:07:12 -0600

    se resolvio el ejercicio07 mediante recursividad de cola

--- ejercicios/Andron (estudiante2)/ejercicio07.py
+++ ejercicios/Andron (estudiante2)/ejercicio07.py
@@ -15,3 +15,16 @@ Casos de prueba:
 • separar_por_paridad(97531) debe retornar [0, 97531] 
 • separar_por_paridad(2468) debe retornar [2468, 0]
 '''
+#formar numeros de manera independiente, sumarlos en forma de lista
+def separar_por_paridad(numero, resPAR=0, resIMPAR=0, expPAR=0, expIMPAR=0):
+    if numero == 0:
+        return [resPAR] + [resIMPAR]
+    if (numero % 10) % 2 == 0: #PAR
+        return separar_por_paridad(numero // 10, resPAR + (numero % 10) * (10**expPAR), resIMPAR, expPAR + 1, expIMPAR)
+    if (numero % 10) % 2 != 0: #IMPAR
+        return separar_por_paridad(numero // 10, resPAR, resIMPAR + (numero % 10) * (10**expIMPAR), expPAR, expIMPAR + 1)  
+
+print(separar_por_paridad(123456))
+print(separar_por_paridad(80231))
+print(separar_por_paridad(97531))
+print(separar_por_paridad(2468))
