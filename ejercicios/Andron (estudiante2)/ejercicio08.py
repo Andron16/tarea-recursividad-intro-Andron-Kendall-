14c411aeb02992915974b875684f76b5a259881e
Andron <andronumana050@gmail.com>
2026-06-10 20:33:02 -0600

    se ha resuelto el ejercicio 08 mediante recursividad de pila

--- ejercicios/Andron (estudiante2)/ejercicio08.py
+++ ejercicios/Andron (estudiante2)/ejercicio08.py
@@ -13,3 +13,15 @@ Casos de prueba:
 • detectar_valles([9, 4, 8, 2, 6]) debe retornar [[9, 4, 8], [8, 2, 6]] 
 • detectar_valles([3, 1]) debe retornar []
 '''
+
+def detectar_valles(lista):
+    if len(lista) < 3:
+        return []
+    if lista[1] < lista[0] and lista[1] < lista[2]:
+        return [[lista[0], lista[1], lista[2]]] + detectar_valles(lista[1:])
+    return detectar_valles(lista[1:])
+
+print(detectar_valles([5, 1, 6, 3, 8, 2, 7]))
+print(detectar_valles([1, 2, 3, 4, 5]))
+print(detectar_valles([9, 4, 8, 2, 6]))
+print(detectar_valles([3, 1]))
