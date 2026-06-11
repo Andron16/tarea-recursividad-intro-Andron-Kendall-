5939f3f34f5f0c619367b01fc9a7095606d60002
Andron <andronumana050@gmail.com>
2026-06-10 19:03:26 -0600

    se ha resuelto el ejercicio06 mediante recursividad de cola

--- ejercicios/Andron (estudiante2)/ejercicio06.py
+++ ejercicios/Andron (estudiante2)/ejercicio06.py
@@ -13,3 +13,17 @@ Casos de prueba:
 • contar_bloques_iguales([1, 2, 3, 4]) debe retornar 4 
 • contar_bloques_iguales([]) debe retornar 0
 '''
+
+def contar_bloques_iguales(lista, res=0):
+    if lista == []:
+        return 0
+    if len(lista) == 1:
+        return res + 1
+    if lista[0] == lista[1]:
+        return contar_bloques_iguales(lista[1:], res)
+    return contar_bloques_iguales(lista[1:], res + 1)
+
+print(contar_bloques_iguales([1,1,2,2,2,3,1,1]))
+print(contar_bloques_iguales([5,5,5,5]))
+print(contar_bloques_iguales([1,2,3,4]))
+print(contar_bloques_iguales([]))
